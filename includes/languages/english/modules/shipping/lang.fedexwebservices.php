12d922483a3f13a00258b2709034465700c6173b
njcyx <njcyx6649250@msn.com>
2023-01-13 16:50:10 -0500

    Update lang.fedexwebservices.php

--- includes/languages/english/modules/shipping/lang.fedexwebservices.php
+++ includes/languages/english/modules/shipping/lang.fedexwebservices.php
@@ -1,10 +1,17 @@
-<?php //modified for zc 1.58
+// Factory file from numinix v1.9.0
+<?php
 $define = [
-	'MODULE_SHIPPING_FEDEX_WEB_SERVICES_TEXT_TITLE' => 'FedEx',
-	'MODULE_SHIPPING_FEDEX_WEB_SERVICES_TEXT_DESCRIPTION' => '<h2>FedEx Web Services</h2><p>You will need to have registered an account with FedEx and proper approval from FedEx identity to use this module. Please see the README.TXT file for other requirements.</p>',
-	'MODULE_SHIPPING_FEDEX_WEB_SERVICES_TEXT_DESCRIPTION_SOAP' => '<h2>FedEx Web Services</h2><p><span style="color:#dd0000;">Warning: SOAP Extension is not enabled. FedEx Web Services Shipping Module will not work until SOAP is enabled. Ensure that PHP is compiled with SOAP. Speak to your hosting company if unsure.</span></p> <p>You will need to have registered an account with FedEx and proper approval from FedEx identity to use this module. Please see the README.TXT file for other requirements.</p>',
-	'MODULE_SHIPPING_FEDEX_WEB_SERVICES_TEXT_SATURDAY' => 'Saturday Delivery',
-
+    'MODULE_SHIPPING_FEDEX_WEB_SERVICES_TEXT_TITLE' => 'FedEx',
+    'MODULE_SHIPPING_FEDEX_WEB_SERVICES_TEXT_DESCRIPTION' => '<h2>FedEx Web Services</h2><p>You will need to have registered an account with FedEx and proper approval from FedEx identity to use this module. Please see the README.TXT file for other requirements.</p>',
+    'MODULE_SHIPPING_FEDEX_WEB_SERVICES_TEXT_DESCRIPTION_SOAP' => '<h2>FedEx Web Services</h2><p><span style="color:#dd0000;">Warning: SOAP Extension is not enabled. FedEx Web Services Shipping Module will not work until SOAP is enabled. Ensure that PHP is compiled with SOAP. Speak to your hosting company if unsure.</span></p> <p>You will need to have registered an account with FedEx and proper approval from FedEx identity to use this module. Please see the README.TXT file for other requirements.</p>',
+    'MODULE_SHIPPING_FEDEX_WEB_SERVICES_TEXT_SATURDAY' => 'Saturday Delivery',
+    'MODULE_SHIPPING_FEDEX_WEB_SERVICES_SORT_ORDER' => 'Order',
+    'MODULE_SHIPPING_FEDEX_WEB_SERVICES_FREE_SHIPPING' => 'Free Shipping',
+    'MODULE_SHIPPING_FEDEX_WEB_SERVICES_STATUS' => 'Status',
+    'MODULE_SHIPPING_FEDEX_WEB_SERVICES_TAX_CLASS' => 'Tax Class',
+    'MODULE_SHIPPING_FEDEX_WEB_SERVICES_ACT_NUM' => 'Fedex Account Number',
+    'MODULE_SHIPPING_FEDEX_WEB_SERVICES_METER_NUM' => 'Fedex Meter Number',
 ];
 
 return $define;
+// eof
